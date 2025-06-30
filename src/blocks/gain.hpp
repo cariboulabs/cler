07bce65694d08c1537c1e227aff546a3fed33e2c
alon <alon@cariboulabs.co>
2025-06-30 14:07:26 +0300

    up

--- src/blocks/gain.hpp
+++ src/blocks/gain.hpp
@@ -1,30 +1,31 @@
 #include "cler.hpp"
 
-template <typename T, size_t BufferSize, size_t WorkSamples>
+template <typename T>
 struct GainBlock : public cler::BlockBase<GainBlock> {
-    cler::Channel<T, BufferSize> in;
-    T gain;
+    cler::Channel<T> in;
 
-    GainBlock(const char* name, T gain_value) :
-     BlockBase(name), gain(gain_value) {}
+    GainBlock(const char* name, T gain_value, size_t buffer_size, size_t work_size):
+     BlockBase(name), gain(gain_value), in(buffer_size), _work_size(work_size) {}
 
     cler::Result<cler::Empty, ClerError> procedure_impl(cler::Channel<T>* out) {
-        if (in.size() < WorkSamples) {
+        if (in.size() < work_size) {
             return ClerError::NotEnoughSamples;
         }
-        if (out->space() < WorkSamples) {
+        if (out->space() < work_size) {
             return ClerError::NotEnoughSpace;
         }
 
-        size_t read = in.readN(_tmp, WorkSamples);
-        for (size_t i = 0; i < WorkSamples; ++i) {
+        size_t read = in.readN(_tmp, work_size);
+        for (size_t i = 0; i < work_size; ++i) {
             _tmp[i] *= gain;
         }
-        size_t written = out->writeN(_tmp, WorkSamples);
+        size_t written = out->writeN(_tmp, work_size);
 
         return cler::Empty{};
     }
 
     private:
-        float _tmp[WorkSamples] = {0.0f};
+        T _gain;
+        float _tmp[work_size] = {0.0f};
+        size_t _work_size
 };
