eb9468c90d397c352f37d265462075012276649c
alonspinner <alonspinner@gmail.com>
2025-07-05 23:54:53 +0300

    got something started...

---
+++ blocks/plot_cspectogram.hpp
@@ -0,0 +1,146 @@
+#pragma once
+#include "cler.hpp"
+#include "gui/gui_manager.hpp"
+#include "liquid.h"
+#include <complex>
+
+struct PlotCSpectrogramBlock : public cler::BlockBase {
+    cler::Channel<std::complex<float>>* in;
+
+    PlotCSpectrogramBlock(const char* name, size_t num_inputs, const char** signal_labels, size_t sps, size_t buffer_size, size_t tall) 
+        : BlockBase(name), _num_inputs(num_inputs), _signal_labels(signal_labels), _sps(sps), _buffer_size(buffer_size), _tall(tall)
+    {
+        if (num_inputs < 1) {
+            throw std::invalid_argument("PlotCSpectrogramBlock requires at least one input channel");
+        }
+        if (buffer_size <= 2 || tall < 1) {
+            throw std::invalid_argument("Buffer size and tall must be > 0");
+        }
+
+        // Allocate input channels
+        in = static_cast<cler::Channel<std::complex<float>>*>(
+            ::operator new[](num_inputs * sizeof(cler::Channel<std::complex<float>>))
+        );
+        for (size_t i = 0; i < num_inputs; ++i) {
+            new (&in[i]) cler::Channel<std::complex<float>>(2*_buffer_size);
+        }
+
+        // Allocate FFT plan and temporary buffers
+        _liquid_inout = new std::complex<float>[_buffer_size];
+        _tmp_y_buffer = new std::complex<float>[_buffer_size];
+        _tmp_magnitude_buffer = new float[_buffer_size];
+        _fftplan = fft_create_plan(_buffer_size, 
+            reinterpret_cast<liquid_float_complex*>(_liquid_inout),
+            reinterpret_cast<liquid_float_complex*>(_liquid_inout),
+            LIQUID_FFT_FORWARD, 0);
+
+        // Allocate spectrogram buffer for each input: tall x buffer_size
+        _spectrograms = new float*[_num_inputs];
+        for (size_t i = 0; i < _num_inputs; ++i) {
+            _spectrograms[i] = new float[_tall * _buffer_size]();
+        }
+
+        _freq_bins = new float[_buffer_size];
+        for (size_t i = 0; i < _buffer_size; ++i) {
+            _freq_bins[i] = i * (static_cast<float>(_sps) / static_cast<float>(_buffer_size));
+        }
+    }
+
+    ~PlotCSpectrogramBlock() {
+        using ComplexChannel = cler::Channel<std::complex<float>>;
+        for (size_t i = 0; i < _num_inputs; ++i) {
+            in[i].~ComplexChannel();
+        }
+        ::operator delete[](in);
+
+        delete[] _liquid_inout;
+        delete[] _tmp_y_buffer;
+        delete[] _tmp_magnitude_buffer;
+
+        fft_destroy_plan(_fftplan);
+
+        for (size_t i = 0; i < _num_inputs; ++i) {
+            delete[] _spectrograms[i];
+        }
+        delete[] _spectrograms;
+
+        delete[] _freq_bins;
+    }
+
+    cler::Result<cler::Empty, cler::Error> procedure() {
+        size_t available = in[0].size();
+        for (size_t i = 1; i < _num_inputs; ++i) {
+            if (in[i].size() < available) {
+                available = in[i].size();
+            }
+        }
+        if (available < _buffer_size) {
+            return cler::Error::NotEnoughSamples;
+        }
+
+        for (size_t i = 0; i < _num_inputs; ++i) {
+           in[i].readN(_tmp_y_buffer, _buffer_size);
+
+            // Assume we want exactly buffer_size for the FFT
+            memcpy(_liquid_inout, _tmp_y_buffer, _buffer_size * sizeof(std::complex<float>));
+            fft_execute(_fftplan);
+
+            // Compute magnitude
+            for (size_t j = 0; j < _buffer_size; ++j) {
+                float re = _liquid_inout[j].real();
+                float im = _liquid_inout[j].imag();
+                _tmp_magnitude_buffer[j] = 10.0f * log10f(sqrtf(re * re + im * im) + 1e-15f);
+            }
+
+            // Push magnitude row into spectrogram: shift up
+            memmove(
+                _spectrograms[i] + _buffer_size,
+                _spectrograms[i],
+                (_tall - 1) * _buffer_size * sizeof(float)
+            );
+            memcpy(
+                _spectrograms[i],
+                _tmp_magnitude_buffer,
+                _buffer_size * sizeof(float)
+            );
+        }
+        return cler::Empty{};
+    }
+
+    void render() {
+        ImGui::Begin(name());
+
+        for (size_t i = 0; i < _num_inputs; ++i) {
+            // ImPlot::SetNextAxesLimits(0, _buffer_size, 0, _tall, ImGuiCond_Always);
+            if (ImPlot::BeginPlot(_signal_labels[i])) {
+                ImPlot::PlotHeatmap(
+                    _signal_labels[i],
+                    _spectrograms[i],
+                    _buffer_size,
+                    _tall,
+                    -160.0f,  // scale min
+                    30.0f, // scale max
+                    nullptr // label format
+                );
+                ImPlot::EndPlot();;
+            }
+        }
+
+        ImGui::End();
+    }
+
+private:
+    size_t _num_inputs;
+    const char** _signal_labels;
+    size_t _sps;
+    size_t _buffer_size;
+    size_t _tall;
+
+    std::complex<float>* _liquid_inout;
+    std::complex<float>* _tmp_y_buffer;
+    float* _tmp_magnitude_buffer;
+    float** _spectrograms;
+
+    fftplan _fftplan;
+    float* _freq_bins;
+};
--- examples/plots.cpp
+++ examples/plots.cpp
@@ -7,6 +7,7 @@
 #include "blocks/math_complex_demux.hpp"
 #include "blocks/plot_cspectrum.hpp"
 #include "blocks/fanout.hpp"
+#include "blocks/plot_cspectogram.hpp"
 #include <complex>
 
 int main() {
@@ -16,7 +17,7 @@ int main() {
     
     SourceCWBlock<std::complex<float>> cw_source("CWSource", 1.0f, 2.0f, SPS);
     ThrottleBlock<std::complex<float>> cw_throttle("CWThrottle", SPS);
-    FanoutBlock<std::complex<float>> cw_fanout("CWFanout", 2);
+    FanoutBlock<std::complex<float>> cw_fanout("CWFanout", 3);
     ComplexToMagPhaseBlock cw_complex2realimag("CWComplex2RealImag", ComplexToMagPhaseBlock::Mode::RealImag);
     const char* signal_labels[] = {"Real", "Imaginary"};
     PlotTimeSeriesBlock cw_timeseries_plot(
@@ -29,7 +30,7 @@ int main() {
 
     SourceChirpBlock<std::complex<float>> chirp_source("ChirpSource", 1.0f, 5.0f, 95.0f, SPS, 10.0f);
     ThrottleBlock<std::complex<float>> chirp_throttle("ChirpThrottle", SPS);
-    FanoutBlock<std::complex<float>> chirp_fanout("ChirpFanout", 2);
+    FanoutBlock<std::complex<float>> chirp_fanout("ChirpFanout", 3);
     ComplexToMagPhaseBlock chirp_c2realimag("ChirpComplex2RealImag", ComplexToMagPhaseBlock::Mode::RealImag);
     PlotTimeSeriesBlock chirp_timeseries_plot(
         "Chirp-TimeSeriesPlot",
@@ -48,20 +49,30 @@ int main() {
         256 // buffer size for FFT
     );
 
+    PlotCSpectrogramBlock cspectrogram_plot(
+        "CW-SpectrogramPlot",
+        2, // number of inputs
+        cspectrum_labels,
+        SPS,
+        256, // buffer size for FFT
+        100 // tall
+    );
+
 
     cler::BlockRunner cw_source_runner(&cw_source, &cw_throttle.in);
     cler::BlockRunner cw_throttle_runner(&cw_throttle, &cw_fanout.in);
-    cler::BlockRunner cw_fanout_runner(&cw_fanout, &cw_complex2realimag.in, &cspectrum_plot.in[0]);
+    cler::BlockRunner cw_fanout_runner(&cw_fanout, &cw_complex2realimag.in, &cspectrum_plot.in[0], &cspectrogram_plot.in[0]);
     cler::BlockRunner cw_complex2realimag_runner(&cw_complex2realimag, &cw_timeseries_plot.in[0], &cw_timeseries_plot.in[1]);
     cler::BlockRunner cw_timeseries_plot_runner(&cw_timeseries_plot);
 
     cler::BlockRunner chirp_source_runner(&chirp_source, &chirp_throttle.in);
     cler::BlockRunner chirp_throttle_runner(&chirp_throttle, &chirp_fanout.in);
-    cler::BlockRunner chirp_fanout_runner(&chirp_fanout, &chirp_c2realimag.in, &cspectrum_plot.in[1]);
+    cler::BlockRunner chirp_fanout_runner(&chirp_fanout, &chirp_c2realimag.in, &cspectrum_plot.in[1], &cspectrogram_plot.in[1]);
     cler::BlockRunner chirp_complex2realimag_runner(&chirp_c2realimag, &chirp_timeseries_plot.in[0], &chirp_timeseries_plot.in[1]);
     cler::BlockRunner chirp_timeseries_plot_runner(&chirp_timeseries_plot);
     
     cler::BlockRunner cspectrum_plot_runner(&cspectrum_plot);
+    cler::BlockRunner cspectrogram_plot_runner(&cspectrogram_plot);
 
     cler::FlowGraph flowgraph(
         cw_source_runner,
@@ -76,7 +87,8 @@ int main() {
         chirp_complex2realimag_runner,
         chirp_timeseries_plot_runner,
 
-        cspectrum_plot_runner
+        cspectrum_plot_runner,
+        cspectrogram_plot_runner
     );
 
     flowgraph.run();
@@ -87,6 +99,7 @@ int main() {
         cw_timeseries_plot.render();
         chirp_timeseries_plot.render();
         cspectrum_plot.render();
+        cspectrogram_plot.render();
         gui.end_frame();
         std::this_thread::sleep_for(std::chrono::milliseconds(20));
     }
