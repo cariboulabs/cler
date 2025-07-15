c202daa16f6a74438f50131e00f7018c02efaa1a
alon <alon@cariboulabs.co>
2025-07-15 15:53:21 +0300

    fixed rendering

--- examples/cariboulite_receiver.cpp
+++ examples/cariboulite_receiver.cpp
@@ -1,6 +1,8 @@
 #include "cler.hpp"
+#include "cler_addons.hpp"
 #include "blocks/sources/source_cariboulite.hpp"
 #include "blocks/plots/plot_cspectrum.hpp"
+#include "gui_manager.hpp"
 
 int main() {
     const size_t samp_rate = 4'000'000; // 4 MHz
@@ -14,21 +16,33 @@ int main() {
         true
     );
 
-    PlotCSpectrumBlock plot_cspectrum(
+    PlotCSpectrumBlock plot(
         "Spectrum Plot",
         {"caribou_signal"},
         samp_rate,
         256);
 
     cler::FlowGraph flowgraph(
-        cler::BlockRunner(&source_cariboulite, &plot_cspectrum.in[0]),
-        cler::BlockRunner(&plot_cspectrum)
+        cler::BlockRunner(&source_cariboulite, &plot.in[0]),
+        cler::BlockRunner(&plot)
     );
 
-    flowgraph.run();
+    cler::GuiManager gui(800, 400, "Hackrf Receiver Example");
+    plot.set_initial_window(0.0f, 0.0f, 800.0f, 400.0f);
 
-    while (true) {
-        // Simulate some work in the main thread
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+    flowgraph.run(cler::FlowGraphConfig{
+        .adaptive_sleep = true,
+    });
+
+
+    while (gui.should_close() == false) {
+        gui.begin_frame();
+        plot.render();
+        gui.end_frame();
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
     }
+    flowgraph.stop();
+    print_flowgraph_execution_report(flowgraph);
+
+    return 0;
 }
--- examples/hackrf_receiver.cpp
+++ examples/hackrf_receiver.cpp
@@ -1,6 +1,8 @@
 #include "cler.hpp"
+#include "cler_addons.hpp"
 #include "blocks/sources/source_hackrf.hpp"
 #include "blocks/plots/plot_cspectrum.hpp"
+#include "gui_manager.hpp"
 
 int main() {
     const uint32_t samp_rate = 4'000'000; // 4 MHz
@@ -12,7 +14,7 @@ int main() {
         samp_rate
     );
 
-    PlotCSpectrumBlock plot_cspectrum(
+    PlotCSpectrumBlock plot(
         "Spectrum Plot",
         {"hackrf_signal"},
         samp_rate,
@@ -20,14 +22,26 @@ int main() {
     );
 
     cler::FlowGraph flowgraph(
-        cler::BlockRunner(&source_hackrf, &plot_cspectrum.in[0]),
-        cler::BlockRunner(&plot_cspectrum)
+        cler::BlockRunner(&source_hackrf, &plot.in[0]),
+        cler::BlockRunner(&plot)
     );
 
-    flowgraph.run();
+    cler::GuiManager gui(800, 400, "Hackrf Receiver Example");
+    plot.set_initial_window(0.0f, 0.0f, 800.0f, 400.0f);
 
-    while (true) {
-        // Keep the main thread alive
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+    flowgraph.run(cler::FlowGraphConfig{
+        .adaptive_sleep = true,
+    });
+
+
+    while (gui.should_close() == false) {
+        gui.begin_frame();
+        plot.render();
+        gui.end_frame();
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
     }
+    flowgraph.stop();
+    print_flowgraph_execution_report(flowgraph);
+
+    return 0;
 }
