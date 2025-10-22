543b0dca56db10d24a2f520aea88f73f9183a6b2
alon <alon@cariboulabs.co>
2025-10-22 18:17:03 +0300

    up

--- desktop_blocks/adsb/libmodes/mode-s.h
+++ desktop_blocks/adsb/libmodes/mode-s.h
@@ -77,7 +77,7 @@ extern "C" {
 
 void mode_s_init(mode_s_t *self);
 void mode_s_compute_magnitude_vector(unsigned char *data, uint16_t *mag, uint32_t size);
-void mode_s_detect(mode_s_t *self, const uint16_t *mag, uint32_t maglen, mode_s_callback_t cb, void* cb_context);
+void mode_s_detect(mode_s_t *self, uint16_t *mag, uint32_t maglen, mode_s_callback_t cb, void* cb_context);
 void mode_s_decode(mode_s_t *self, struct mode_s_msg *mm, unsigned char *msg);
 
 #ifdef __cplusplus
